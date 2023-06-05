a0f1a58a7eb65c45d99ba5a17a4b5b4a938ca84c
buiam <matteo.buia@linksmt.it>
2023-06-05 16:56:03 +0200

    Add Swagger

--- src/main/java/com/example/demojava11/Demojava11Application.java
+++ src/main/java/com/example/demojava11/Demojava11Application.java
@@ -3,11 +3,15 @@ package com.example.demojava11;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
+@RestController("/")
 public class Demojava11Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
@@ -19,5 +23,8 @@ public class Demojava11Application implements CommandLineRunner {
 	public void run(String... args) {
 		log.info("StringBuilder: [{}]", new StringBuffer().toString());
 	}
+
+	@GetMapping("test")
+	public String test() {log.info("get controller"); return "test response";}
 
 }
